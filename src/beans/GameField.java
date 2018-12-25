73e32773290170586811ece9516b7d0c6a0c3b8e
Yuri Nevenchenov <nevenchenov@gmail.com>
2018-12-25 21:16:00 +0200

    created beans

---
+++ src/beans/GameField.java
@@ -0,0 +1,43 @@
+package beans;
+
+public class GameField {
+    private int width;
+    private int height;
+    private char[][] gameField;
+
+    public GameField(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        gameField = new char[width][height];
+        fillGameField();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getGameField() {
+        return gameField;
+    }
+
+    public void fillCell(int x, int y, char value) {
+        gameField[x][y] = value;
+    }
+
+    public char getCellValue(int x, int y) {
+        return gameField[x][y];
+    }
+
+    private void fillGameField() {
+        for(int i = 0; i < getHeight(); i++){
+            for(int j = 0; j < getWidth(); j++){
+                fillCell(j,i, '_');
+            }
+        }
+    }
+}
